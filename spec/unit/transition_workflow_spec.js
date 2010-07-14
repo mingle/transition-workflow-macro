e1c32aa06bf8665e2ac008e3f722c432d5c17795
Li Xiao <swing1979@gmail.com>
2010-07-14 14:56:36 -0700

    started to build workflow text based on xml content

--- spec/unit/transition_workflow_spec.js
+++ spec/unit/transition_workflow_spec.js
@@ -5,24 +5,40 @@ describe 'TransitionWorkflow'
       var transitions = "<transitions type='array'><transition>" + acceptedTransition + "</transition></transitions>"
       var workflow = new TransitionWorkflow;
       var transitions = workflow.parseAllTransitions(toXmldomElement(transitions));
-      1.should_equal(transitions.length);
+      transitions.length.should_equal(1);
 
       transition = transitions[0];
 
-      "Accepted".should_equal(transition.name);
-      "Story".should_equal(transition.card_type.name);
+      transition.name.should_equal('Accepted');
+      transition.card_type.name.should_equal("Story");
 
-      "Status".should_equal(transition.if_card_has_properties[0].name);
-      "Ready for Signoff".should_equal(transition.if_card_has_properties[0].value);
-      "Managed text list".should_equal(transition.if_card_has_properties[0].type_description);
+      transition.if_card_has_properties[0].name.should_equal('Status');
+      transition.if_card_has_properties[0].value.should_equal("Ready for Signoff");
+      transition.if_card_has_properties[0].type_description.should_equal("Managed text list");
 
-      "Status".should_equal(transition.will_set_card_properties[0].name);
-      "Accepted".should_equal(transition.will_set_card_properties[0].value);
-      "Managed text list".should_equal(transition.will_set_card_properties[0].type_description);
+      transition.will_set_card_properties[0].name.should_equal('Status');
+      transition.will_set_card_properties[0].value.should_equal("Accepted");
+      transition.will_set_card_properties[0].type_description.should_equal("Managed text list");
 
-      "Owner".should_equal(transition.will_set_card_properties[1].name);
-      null.should_equal(transition.will_set_card_properties[1].value);
-      "Automatically generated from the team list".should_equal(transition.will_set_card_properties[1].type_description);
+      transition.will_set_card_properties[1].name.should_equal('Owner');
+      transition.will_set_card_properties[1].value.should_equal(null);
+      transition.will_set_card_properties[1].type_description.should_equal("Automatically generated from the team list");
+    end
+    
+    it 'find the starting and ending transitions'
+      // var expected = ["New->Ready for Analysis: Add to Current Sprint", "New->Ready for Analysis: Add to Next Sprint"]
+      // expected.should_equal(transitions.collect(function(transition) {
+      //   return transition.toWorkflow();
+      // }));
+    end
+  end
+  describe 'Transition'
+    it 'as workflow markup'
+      var transitions = "<transitions type='array'><transition>" + acceptedTransition + "</transition></transitions>"
+      var workflow = new TransitionWorkflow;
+      var transitions = workflow.parseAllTransitions(toXmldomElement(transitions));
+      transition = transitions[0];
+      transition.asWorkflowMarkup('Status').should_equal("Ready for Signoff->Accepted: Accepted");
     end
   end
 end
