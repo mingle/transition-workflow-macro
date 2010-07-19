f4be0be46e0b20b10183e492bdbabce93661586d
Li Xiao <swing1979@gmail.com>
2010-07-19 16:57:53 -0700

    added style parameter support and made worflow image container id uniq

--- lib/mingle_plugin_transition_workflow.rb
+++ lib/mingle_plugin_transition_workflow.rb
@@ -1,15 +1,30 @@
 class MinglePluginTransitionWorkflow
+  STYLES = [
+    'default',
+    'earth',
+    'modern-blue',
+    'mscgen',
+    'omegapple',
+    'qsd',
+    'rose',
+    'roundgreen',
+    'napkin'
+  ]
 
   def initialize(parameters, project, current_user)
     @parameters = parameters
     @project = project
     @current_user = current_user
+
+    @style = @parameters['style'].to_s.strip.downcase
+    @style = STYLES.include?(@style) ? @style : 'default'
   end
     
   def execute
+    container_id = "mingle_plugin_transition_workflow#{rand(10000)}"
     html = <<-HTML
 
-<div id="mingle_plugin_transition_workflow">
+<div id="#{container_id}" style="width: 100%; overflow: auto">
 </div>
 
 <script src="/plugin_assets/transition_workflow/javascripts/transition_workflow.js?1279240904" type="text/javascript"></script>
@@ -22,13 +37,13 @@ class MinglePluginTransitionWorkflow
       var card_property = #{@parameters['card-property'].inspect};
 
       var markup = facade.createMarkup(card_type, card_property, '/api/v2/projects/#{@project.identifier}');
-      var div = Builder.node('div', {className: 'wsd' , wsd_style: 'default'});
+      var div = Builder.node('div', {className: 'wsd' , wsd_style: #{@style.inspect}});
       var pre = Builder.node('pre', {style: 'display:none'});
       pre.innerHTML = markup.join("\\n");
       div.appendChild(pre);
       var script = Builder.node('script', {type: 'text/javascript', src: 'http://www.websequencediagrams.com/service.js'});
       div.appendChild(script);
-      $("mingle_plugin_transition_workflow").appendChild(div);
+      $("#{container_id}").appendChild(div);
     } catch (e) {
       console.log(e)
     }
